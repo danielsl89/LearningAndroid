5221ccae649b815db39b3acc5a066a2287a1d646
dani <danielsl89@gmail.com>
2018-04-15 17:58:24 -0300

    Added navigation drawer

--- app/src/main/java/com/example/acamica/recyclegrid/MainActivity.java
+++ app/src/main/java/com/example/acamica/recyclegrid/MainActivity.java
@@ -1,18 +1,26 @@
 package com.example.acamica.recyclegrid;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements MainFragment.OnItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements MainFragment.OnItemSelectedListener, NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +48,18 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnIt
                 }
             });
         }
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        if(drawerLayout != null){
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_navigation_drawer, R.string.close_navigation_drawer);
+            drawerLayout.addDrawerListener(toggle);
+            toggle.syncState();
+        }
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        if(navigationView != null){
+            navigationView.setNavigationItemSelectedListener(this);
+        }
+
     }
 
     @Override
@@ -68,5 +88,24 @@ public class MainActivity extends AppCompatActivity implements MainFragment.OnIt
     @Override
     public void onItemSelected(int id) {
         Log.d("MainActivity", "Item selected: " + id);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.navigation_item_1:
+                Toast.makeText(MainActivity.this, R.string.navigation_item_1, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_item_2:
+                Toast.makeText(MainActivity.this, R.string.navigation_item_2, Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.navigation_item_3:
+                Toast.makeText(MainActivity.this, R.string.navigation_item_3, Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
